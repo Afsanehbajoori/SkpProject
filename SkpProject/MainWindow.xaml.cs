053ae62c6b879b6adcf3eaddae746985226c0264
AfsanehBajoori <afsan.bajoori@hotmail.com>
2021-05-04 13:22:56 +0200

    Add project files.

---
+++ SkpProject/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkpProject
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnHome_Click(object sender, RoutedEventArgs e)
+		{
+			HomePanel.Visibility = Visibility.Visible;
+			UnderviserPanel.Visibility = Visibility.Collapsed;
+			LederPanel.Visibility = Visibility.Collapsed;
+		}
+
+		private void btnUnderviser_Click(object sender, RoutedEventArgs e)
+		{
+			UnderviserPanel.Visibility = Visibility.Visible;
+			HomePanel.Visibility = Visibility.Collapsed;
+			LederPanel.Visibility = Visibility.Collapsed;
+		}
+
+		private void btnLeder_Click(object sender, RoutedEventArgs e)
+		{
+			LederPanel.Visibility = Visibility.Visible;
+			HomePanel.Visibility = Visibility.Collapsed;
+			UnderviserPanel.Visibility = Visibility.Collapsed;
+		}
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void signinButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
